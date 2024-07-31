cd5188e7989b93f5c6f45f7488bce63d377d2984
anna <anka-kudrow@mail.ru>
2024-07-31 19:42:23 +0300

    added stone town page

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const slides = document.querySelectorAll(".slider-item");
+const controlls = document.querySelectorAll(".controlls");
+let slideIndex = 0;
 
+function show(index) {
+  slides[slideIndex].classList.remove("active");
+  slides[index].classList.add("active");
+  slideIndex = index;
+}
+
+controlls.forEach((e) => {
+  e.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("prev")) {
+      let index = slideIndex - 1;
+
+      if (index < 0) {
+        index = slides.length - 1;
+      }
+
+      show(index);
+    } else if (evt.target.classList.contains("next")) {
+      let index = slideIndex + 1;
+      if (index >= slides.length) {
+        index = 0;
+      }
+      show(index);
+    }
+  });
+});
+
+show(slideIndex);
