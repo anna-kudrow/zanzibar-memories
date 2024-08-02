7af0419a92336489680037124978bdf28618f4cd
anna <anka-kudrow@mail.ru>
2024-08-02 13:59:21 +0300

    fixed stowntown area data and added nungwi area

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,16 +1,1 @@
 "use strict";
-
-var _swiperBundleMin = _interopRequireDefault(require("https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var swiper = new _swiperBundleMin["default"](".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
