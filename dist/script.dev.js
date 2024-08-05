38dcd95f591a6067a67b27903840f6c8306468a4
anna <anka-kudrow@mail.ru>
2024-08-05 14:24:13 +0300

    added jozani forest, decorated info block

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,8 +1,1 @@
-// .main__zanzibar-map img:hover {
-//   transform: scale(1.1);
-// }
-// const stoneTownLink = document.querySelector(".flag-stone-town");
-// stoneTownLink.addEventListener("mouseover", (evt) => {
-//   window.location.href = "redirect-url";
-// });
 "use strict";
--- script.js
+++
@@ -1,9 +0,0 @@
-// .main__zanzibar-map img:hover {
-//   transform: scale(1.1);
-// }
-
-// const stoneTownLink = document.querySelector(".flag-stone-town");
-
-// stoneTownLink.addEventListener("mouseover", (evt) => {
-//   window.location.href = "redirect-url";
-// });
